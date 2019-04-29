2e4b87e344d81757eaab5cc9f926a99355fdbfab
markorodic <mrmarkorodic@googlemail.com>
2019-04-29 15:35:25 +0100

    add redux for managing state and setup the store

---
+++ src/provider/RootReducer.js
@@ -0,0 +1,5 @@
+import reduceReducers from "reduce-reducers";
+
+const RootReducer = reduceReducers({});
+
+export default RootReducer;
---
+++ src/provider/Store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { initialState } from "../initialState";
+import RootReducer from "./RootReducer";
+import createSagaMiddleware from "redux-saga";
+import SagaWorkers from "./Sagas.js";
+
+export let store;
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+export default function configureStore() {
+  const sagaMiddleware = createSagaMiddleware();
+  const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+  store = createStore(RootReducer, initialState, enhancer);
+  sagaMiddleware.run(SagaWorkers);
+}
+
+configureStore();
