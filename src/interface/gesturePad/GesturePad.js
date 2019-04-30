8c3f085d45bcc0c071681236a6ac3e3e8e825393
markorodic <mrmarkorodic@googlemail.com>
2019-04-30 23:26:27 +0100

    Get mouse and touch positions on gesture pad

--- src/interface/gesturePad/GesturePad.js
+++ src/interface/gesturePad/GesturePad.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import {
+  getRelativeMousePositionFrom,
+  getRelativeTouchPositionFrom,
+  getElementPosition
+} from "./utils";
 
 class GesturePad extends Component {
+  state = {
+    x: null,
+    y: null,
+    width: null,
+    height: null
+  };
+  gesturePadElement = React.createRef();
+
+  componentDidMount() {
+    const { x, y, width, height } = getElementPosition(this.gesturePadElement);
+
+    this.setState(currentState => ({
+      x,
+      y,
+      width,
+      height
+    }));
+  }
+
+  onMouseMove = e => {
+    e.preventDefault();
+    getRelativeMousePositionFrom(e, this.state);
+  };
+
+  onTouchMove = e => {
+    e.preventDefault();
+    getRelativeTouchPositionFrom(e, this.state);
+  };
+
   render() {
-    return <div className="gesture-pad">Gesture</div>;
+    return (
+      <div
+        onMouseMove={this.onMouseMove}
+        onTouchMove={this.onTouchMove}
+        className="gesture-pad"
+        ref={this.gesturePadElement}
+      >
+        Gesture
+      </div>
+    );
   }
 }
 
