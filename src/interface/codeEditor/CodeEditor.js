f8208c7038f6d8d419b9ad8eb79ddc9df47c64dd
markorodic <mrmarkorodic@googlemail.com>
2019-04-30 14:08:02 +0100

    Add Code Mirror for the code editor

--- src/interface/codeEditor/CodeEditor.js
+++ src/interface/codeEditor/CodeEditor.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/theme/neat.css";
+import "codemirror/mode/xml/xml.js";
+import "codemirror/mode/javascript/javascript.js";
+
+import { Controlled as CodeMirror } from "react-codemirror2";
 
 class CodeEditor extends Component {
+  state = {
+    value: "123"
+  };
   render() {
-    return <div className="code">Code</div>;
+    return (
+      <div className="code">
+        <p>Code</p>
+
+        <CodeMirror
+          value={this.state.value}
+          options={{ lineNumbers: true, autofocus: true }}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({ value });
+          }}
+          onChange={(editor, value) => {
+            console.log("controlled", { value });
+          }}
+        />
+      </div>
+    );
   }
 }
 
