6f77dfbfa7e4686987a301d65351c34a51c0e856
markorodic <mrmarkorodic@googlemail.com>
2019-06-11 09:37:40 +0100

    Remove dead code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, { Component } from "react";
 import Interface from "./interface/Interface";
-import AST from "./ast/AST";
 import { setCSSCustomProperty } from "./utils";
 import { preventRefreshOnMobile } from "./utils";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    loading: false
-  };
   componentWillMount() {
     setCSSCustomProperty();
     preventRefreshOnMobile();
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        loading: false
-      });
-    }, 2000);
-  }
+
   render() {
-    // while checking user session, show "loading" message
-    if (this.state.loading) return SplashScreen();
-
-    // otherwise, show the desired route
-    return (
-      <AST>
-        <Interface />
-      </AST>
-    );
+    return <Interface />;
   }
-}
-
-function SplashScreen(props) {
-  return <div className="splash-screen" />;
 }
 
 export default App;
