ca43109dc723face81a7d48c9495f024d6bf40cb
Marko Rodici <mrmarkorodic@googlemail.com>
2021-05-26 18:45:54 +0100

    Updates

---
+++ precache-manifest.c0cf5b07c3f9264bed3c66d3a6d9da4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70af177bbefc750a0b1223974015782b",
+    "url": "/lines_of_code/index.html"
+  },
+  {
+    "revision": "5aad8edf36ad86b9afba",
+    "url": "/lines_of_code/static/css/2.6f2ee7ef.chunk.css"
+  },
+  {
+    "revision": "b154060f6ca092e0999e",
+    "url": "/lines_of_code/static/css/main.efe1ac76.chunk.css"
+  },
+  {
+    "revision": "5aad8edf36ad86b9afba",
+    "url": "/lines_of_code/static/js/2.fd6f9906.chunk.js"
+  },
+  {
+    "revision": "b154060f6ca092e0999e",
+    "url": "/lines_of_code/static/js/main.8aa8e02d.chunk.js"
+  },
+  {
+    "revision": "4b4ee835db56b798f55b",
+    "url": "/lines_of_code/static/js/runtime~main.b5b5e414.js"
+  },
+  {
+    "revision": "2c2f527ab841f4e29722cd0d510ba205",
+    "url": "/lines_of_code/static/media/email_icon.2c2f527a.svg"
+  },
+  {
+    "revision": "57b20b383298e93d584b0746e4bf75d6",
+    "url": "/lines_of_code/static/media/finish_icon.57b20b38.svg"
+  },
+  {
+    "revision": "94789afc1839c959c485a248c4a1c7a1",
+    "url": "/lines_of_code/static/media/logo.94789afc.svg"
+  },
+  {
+    "revision": "27e32e39ff8cdd8095b4abbdd439994b",
+    "url": "/lines_of_code/static/media/play_icon.27e32e39.svg"
+  },
+  {
+    "revision": "2dc9d234eca23ccd07145c1da46825dc",
+    "url": "/lines_of_code/static/media/restart_icon.2dc9d234.svg"
+  }
+]);
